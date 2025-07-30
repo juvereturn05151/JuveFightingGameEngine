b7348cf6cc60e465986574ed76d6fdacbdb95632
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-07-30 12:03:06 -0700

    add everything

--- WalkUpThrow_Production/Assets/Scripts/WalkUpThrow/CharacterController2D.cs
+++ WalkUpThrow_Production/Assets/Scripts/WalkUpThrow/CharacterController2D.cs
@@ -1,3 +1,4 @@
+using FightinGameEngine;
 using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
@@ -8,8 +9,12 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField]
     private Animator animator;
 
+    [SerializeField] private MoveData moveData;
+    [SerializeField] private HitBoxManager hitboxManager;
+
     private InputHandler inputHandler;
 
+    private bool isAttacking = false;
 
     private void Awake()
     {
@@ -18,29 +23,50 @@ public class CharacterController2D : MonoBehaviour
 
     private void Update()
     {
+        // Prioritize attack over movement
+        if (!isAttacking && inputHandler.CurrentInput.Attack)
+        {
+            Attack();
+            return;
+        }
+
         Vector2 moveDir = inputHandler.CurrentInput.Direction;
 
         // Apply simple horizontal movement
         Vector3 delta = new Vector3(moveDir.x * walkSpeed * Time.deltaTime, 0, 0);
         transform.position += delta;
 
-        // Flip character based on direction
-        if (moveDir.x > 0)
-        {
-            transform.localScale = Vector3.one;
-            animator.Play("WalkForward");
-        }
-        else if (moveDir.x < 0)
-        {
-            animator.Play("WalkBackward");
-            //transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else 
-        {
-            animator.Play("Idle");
-        }
-            
+        // Flip character and handle walk animations
+        //if (moveDir.x > 0)
+        //{
+        //    transform.localScale = Vector3.one;
+        //    animator.Play("WalkForward");
+        //}
+        //else if (moveDir.x < 0)
+        //{
+        //    //transform.localScale = new Vector3(-1, 1, 1);
+        //    animator.Play("WalkBackward");
+        //}
+        //else
+        //{
+        //    animator.Play("Idle");
+        //}
+    }
 
-        // Handle attack/block/throw with MoveExecutor (not shown here)
+    private void Attack()
+    {
+        isAttacking = true;
+        animator.Play("Attack");
+
+        // You can also trigger events on animation using Animation Events
+        // to instantiate a hitbox or return to idle
+
+        // Optionally: use a coroutine to control timing
+        Invoke(nameof(EndAttack), 0.5f); // 0.5s attack duration (tweak as needed)
+    }
+
+    private void EndAttack()
+    {
+        isAttacking = false;
     }
 }
